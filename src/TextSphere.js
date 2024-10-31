ee19cdd21b2bf6ca44499976c143baa6ab30f1ef
OtarMurmanishvili <94814294+Rektovski@users.noreply.github.com>
2024-10-31 20:34:40 +0400

    Update TextSphere.js

--- src/TextSphere.js
+++ src/TextSphere.js
@@ -1,45 +1,55 @@
 import "./testSphere.css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import TagCloud from "TagCloud";
 
 export default function TextShpere() {
+    const containerRef = useRef(null);
+
     useEffect(() => {
+        const texts = [
+            "Windows",
+            "Linux",
+            "C++",
+            "JavaScript",
+            "Matlab",
+            "React",
+            "HTML",
+            "CSS",
+            "SCSS",
+            "SASS",
+            "Bootstrap",
+            "React-Bootstrap",
+            "Chakra UI",
+            "CLION",
+            "Webstorm",
+            "Github",
+            "Figma",
+            "Netlify",
+            "Retool API",
+            "Firebase API",
+        ];
+
+        const options = {
+            radius: 300,
+            maxSpeed: "fast",
+            initSpeed: "fast",
+            keep: true,
+        };
+
+        // Capture the current reference
+        const currentContainer = containerRef.current;
+
+        if (currentContainer) {
+            TagCloud(currentContainer, texts, options);
+        }
+
+        // Cleanup function
         return () => {
-            const container = ".tagcloud";
-            const texts = [
-                "Windows",
-                "Linux",
-                "C++",
-                "JavaScript",
-                "Matlab",
-                "React",
-                "HTML",
-                "CSS",
-                "SCSS",
-                "SASS",
-                "Bootstrap",
-                "React-Boostrap",
-                "Chakra UI",
-                "CLION",
-                "Webstorm",
-                "Github",
-                "Figma",
-                "Netlify",
-                "Retool API",
-                "Firebase API",
-            ];
-
-            const options = {
-                radius: 300,
-                maxSpeed: "fast",
-                initSpeed: "fast",
-                keep: true,
-            };
-
-            TagCloud(container, texts, options);
+            if (currentContainer) {
+                currentContainer.innerHTML = ""; // Clear the content
+            }
         };
     }, []);
-
     return (
         <>
             <div className="text-sphere">
